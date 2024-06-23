b836d38fa15b5c12031635d0171ba9fce4acb039
KleinKodes <kleinharrigan@gmail.com>
2024-06-23 15:23:41 -0400

    set something and added comment.

---
+++ Assets/Scripts/nameSelector.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nameSelector : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+    // Start is called before the first frame update
+
+
+    public void selectName() 
+    {
+        if (inputField != null)
+        {
+            GlobalReferences.PLAYER.Username = inputField.text;
+            Debug.Log("set name to: " + inputField.text);
+            SceneManager.UnloadSceneAsync(SceneNames.NAMESELECTOR);
+
+            if (GlobalEvents.FullPlaythroughInProgress.Invoked())
+            {
+                SceneManager.LoadSceneAsync(SceneNames.LEVELCONTROLLER, mode: LoadSceneMode.Additive);
+                GlobalReferences.LEVELMANAGER.setLevel(1);
+            }
+            SceneManager.LoadSceneAsync(SceneNames.LEVELSELECTOR, mode:LoadSceneMode.Additive);
+
+        }
+    }
+}
