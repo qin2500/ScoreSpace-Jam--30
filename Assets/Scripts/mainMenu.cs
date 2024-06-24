f5d8c8431958ada73b73ef0f0899c10320bc352d
KleinKodes <kleinharrigan@gmail.com>
2024-06-23 23:35:50 -0400

    fixed conflicts.

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -8,52 +8,30 @@ public class mainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
 
-<<<<<<< HEAD
-    [SerializeField] GameObject main;
-    //[SerializeField] GameObject levelSelect;
-    //[SerializeField] GameObject credits;
-=======
     [SerializeField] GameObject canvas;
->>>>>>> cb1fb91b87be85ae9616a449223ff5d896e7a540
 
     GameObject currentlyActive;
 
-    private void Awake()
-    {
-        currentlyActive = main;
-    }
     public void Play()
     {
         GlobalEvents.FullPlaythroughInProgress.invoke();
         SceneManager.LoadSceneAsync("NameSelector", mode: LoadSceneMode.Additive);
-<<<<<<< HEAD
-        currentlyActive.SetActive(false);
         
-=======
         hideAssets();
->>>>>>> cb1fb91b87be85ae9616a449223ff5d896e7a540
     }
 
     public void levelSelector()
     {
         GlobalEvents.FullPlaythroughInProgress.uninvoke();
         SceneManager.LoadSceneAsync("NameSelector", mode: LoadSceneMode.Additive);
-<<<<<<< HEAD
-        currentlyActive.SetActive(false);
 
-=======
         hideAssets();
->>>>>>> cb1fb91b87be85ae9616a449223ff5d896e7a540
     }
 
     public void loadCredits()
     {
-<<<<<<< HEAD
-        currentlyActive.SetActive(false);
-=======
         hideAssets();
->>>>>>> cb1fb91b87be85ae9616a449223ff5d896e7a540
-        SceneManager.LoadSceneAsync("Credits", mode: LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(SceneNames.CREDITS, mode: LoadSceneMode.Additive);
     }
 
     public void loadLeaderBoard()
